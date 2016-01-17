db530248f32aa12e8967edfb68a0d482680fdd22
Germain Boue <boue.germain@gmail.com>
2016-01-17 11:31:57 -0800

    Unit test for the logic of taking two entries under vote

--- src/core.js
+++ src/core.js
@@ -1,6 +1,17 @@
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 //Sets an entries key in the state Map, and set the value as the given List entries
 export function setEntries(state, entries){
   return state.set('entries', List(entries));
+}
+
+export function next(state){
+  //.get Returns the value associated with the provided key
+  const entries = state.get('entries');
+  return state.merge({
+    //takes first two
+    vote: Map({pair: entries.take(2)}),
+    //returns entries but by skipping first two
+    entries: entries.skip(2)
+  });
 }
